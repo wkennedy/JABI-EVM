7eedb8cdf251cfa3010c6228eff574245fee7174
Will Kennedy <will.n.kennedy@gmail.com>
2024-05-27 23:15:13 -0400

    Updates for Actions (Publish)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     jacoco
+    `maven-publish`
 }
 
 group = "com.github.wkennedy"
@@ -39,5 +40,23 @@ tasks.jacocoTestReport {
         }
     }
     dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
